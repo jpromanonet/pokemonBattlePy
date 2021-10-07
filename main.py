7de810aa0b87897a60b7f05362b9689418b04165
jpromanonet <programming@jpromano.net>
2021-10-06 23:36:40 -0300

    Main

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ from models import *
 
 # Primero tenemos que definir los stats y los pokemon
 
-pokemon1 = Pokemon("")
+pokemon1 = Pokemon("Bulbasaur", 100, "grass", "poison")
+pokemon2 = Pokemon("Charmander", 100, "fire", None)
+
+# Stats
+pokemon1.stats = {
+    HP: 45,
+    
+}
