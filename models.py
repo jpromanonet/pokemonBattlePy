ca0ca4182bbce1cda35d7f6d68efeb430652177f
jpromanonet <programming@jpromano.net>
2021-10-08 18:41:22 -0300

    Function battle

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-# Este archivo tiene las clases que modelan a nuestros pokemon que van a pelear entre si
+from constants import *
+
 class Battle:
 
     def __init__(self, pokemon1, pokemon2):
@@ -7,16 +8,48 @@ class Battle:
         self.actual_turn = 0
 
     def is_finished(self):
-        return self.pokemon1.current_hp <= 0 or self.pokemon2.current_hp <= 0
+        finished = self.pokemon1.current_hp <= 0 or self.pokemon2.current_hp <= 0
+        if finished:
+            self.print_winner()
+        return finished
+    
+    def print_winner(self):
+        if self.pokemon1.current_hp <= 0 < self.pokemon2.current_hp:
+            print(self.pokemon2.name + " won in " + str(self.actual_turn)+" turns!!")
+        elif self.pokemon2.current_hp <= 0 < self.pokemon1.current_hp:
+            print(self.pokemon1.name + " won in " + str(self.actual_turn)+" turns!!")
+        else:
+            print("DOUBLE KO")
+
+    def execute_turn(self, turn):
+        # Logica para ejecutar un turno dentro de una batalla
+        command1 = turn.command1
+        command2 = turn.command2
+        attack1 = None
+        attack2 = None
+        if DO_ATTACK in command1.action.keys():
+            attack1 = self.pokemon1.attacks[command1.action[DO_ATTACK]]
+        if DO_ATTACK in command2.action.keys():
+            attack2 = self.pokemon2.attacks[command2.action[DO_ATTACK]]
+
+        # Ejecutar ataques
+        self.pokemon2.current_hp -= attack1.power
+        self.pokemon1.current_hp -= attack2.power
+        self.actual_turn += 1
+
+    def print_current_status(self):
+        print(self.pokemon1.name + " has " + str(self.pokemon1.current_hp) + " left!")
+        print(self.pokemon2.name + " has " + str(self.pokemon2.current_hp) + " left!")
 
 class Pokemon:
+
     def __init__(self, name, level, type1, type2):
-        self.name = name
-        self.level = level
-        self.type1 = type1
         self.type2 = type2
-        self.attacks = [ ] # Con el hashtag ponemos comentarios y los arrays se definen con corchetes igual que en JS
-        self.stats = {} # con las llaves definimos tuplas
+        self.type1 = type1
+        self.level = level
+        self.name = name
+        self.attacks = []
+        self.stats = {}
         self.current_status = 0
         self.current_hp = 0
 
@@ -35,10 +68,11 @@ class Turn:
     def __init__(self):
         self.command1 = None
         self.command2 = None
-
+    
     def can_start(self):
-        return self.command1
+        return self.command1 is not None and self.command2 is not None
 
 class Command:
+
     def __init__(self, action):
         self.action = action
