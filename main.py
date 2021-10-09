7a0016eae02a7cc83d813bb84bb424793e96b99c
jpromanonet <programming@jpromano.net>
2021-10-09 17:46:08 -0300

    Basic function

--- main.py
+++ main.py
@@ -1,45 +1,24 @@
 from constants import *
-from models.Battle import *
-from models.Pokemon import *
+from models import *
 
-# Primero tenemos que definir los stats y los pokemon
+#First define pokemons with its stats
 
 pokemon1 = Pokemon("Bulbasaur", 100, "grass", "poison")
 pokemon2 = Pokemon("Charmander", 100, "fire", None)
 pokemon1.current_hp = 45
 pokemon2.current_hp = 39
+#Stats
 
-pokemon1.baseStats = {
-    HP: 108,
-    ATTACK: 130,
-    DEFENSE: 95,
-    SPATTACK: 80,
-    SPDEFENSE: 85,
-    SPEED: 102
+pokemon1.stats = {
+    HP: 45,
+    ATTACK: 49,
+    DEFENSE: 49,
+    SPATTACK: 65,
+    SPDEFENSE: 65,
+    SPEED: 45
 }
 
-pokemon1.ev = {
-    HP: 74,
-    ATTACK: 190,
-    DEFENSE: 91,
-    SPATTACK: 48,
-    SPDEFENSE: 84,
-    SPEED: 23
-}
-
-pokemon1.iv = {
-    HP: 24,
-    ATTACK: 190,
-    DEFENSE: 91,
-    SPATTACK: 48,
-    SPDEFENSE: 84,
-    SPEED: 23
-}
-
-pokemon1.compute_stats()
-print(pokemon1.stats)
-
-pokemon2.baseStats = {
+pokemon2.stats = {
     HP: 39,
     ATTACK: 52,
     DEFENSE: 43,
@@ -48,54 +27,39 @@ pokemon2.baseStats = {
     SPEED: 65
 }
 
-pokemon2.ev = {
-    HP: 0,
-    ATTACK: 0,
-    DEFENSE: 0,
-    SPATTACK: 0,
-    SPDEFENSE: 0,
-    SPEED: 0
-}
-
-pokemon2.iv = {
-    HP: 21,
-    ATTACK: 21,
-    DEFENSE: 21,
-    SPATTACK: 21,
-    SPDEFENSE: 21, 
-    SPEED: 21
-}
-
-# Attacks
+#Attacks
 pokemon1.attacks = [Attack("scratch", "normal", PHYSICAL, 10, 10, 100)]
 pokemon2.attacks = [Attack("scratch", "normal", PHYSICAL, 10, 10, 100)]
+
 
 def ask_command(pokemon):
     command = None
     while not command:
-        tmp_command = input("What should "+pokemon.name+" do?").split(" ")
+        tmp_command = input("What should "+pokemon.name+" do? ").split(" ")
         if len(tmp_command) == 2:
             try:
                 if tmp_command[0] == DO_ATTACK and 0 <= int(tmp_command[1]) < 4:
                     command = Command({DO_ATTACK: int(tmp_command[1])})
             except Exception:
-                    pass
-    return command 
+                pass
+    return command
 
-# Comenzar batalla
+#Start battle
+
 battle = Battle(pokemon1, pokemon2)
 
 while not battle.is_finished():
-    # First ask for command
+    #Main pokemon battle loop
+    #First ask for the commands
     command1 = ask_command(pokemon1)
     command2 = ask_command(pokemon2)
 
-    # Genera un nuevo turno =)
+    #Generate new turn
     turn = Turn()
     turn.command1 = command1
     turn.command2 = command2
 
     if turn.can_start():
-        # Ejecutar turno
+        # Execute turn
         battle.execute_turn(turn)
-        battle.print_current_status()    
+        battle.print_current_status()
